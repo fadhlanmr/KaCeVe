780851c323800c7cfc911bf3066d64f29ec55a7f
Muhammad Fadhlan Min Robby <fadhlan.mr@gmail.com>
2018-03-31 20:27:43 +0700

    Left, and right command
    bonus rotation bug, too lazy to fix it, a fun bug anyway

--- Assets/scripts/snakeOne.cs
+++ Assets/scripts/snakeOne.cs
@@ -6,8 +6,12 @@ public class snakeOne : MonoBehaviour {
 
 	bool flagUp 	= false;
 	bool flagDown 	= false;
+    bool flagRight  = false;
+    bool flagLeft   = false;
+    bool rotated    = false;
 
 	public Vector2 Position;
+    public Sprite facing;
 	public Vector2 Velocity;
 
 	// Use this for initialization
@@ -20,23 +24,65 @@ public class snakeOne : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			flagUp = true;
-
 			flagDown = false;
-		}
+            flagLeft = false;
+            flagRight = false;
+            rotated = false;
+        }
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			flagDown = true;
-
 			flagUp = false;
-		}
+            flagLeft = false;
+            flagRight = false;
+            rotated = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            flagRight = true;
+            flagLeft = false;
+            flagDown = false;
+            flagUp = false;
+            rotated = false;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            flagRight = false;
+            flagLeft = true;
+            flagDown = false;
+            flagUp = false;
+            rotated = false;
+        }
 
-		if (flagUp) {
+        if (flagUp) {
 			transform.position = Position;
-			Position.y += 0.05F;
+			Position.y += 0.1F;
+            this.gameObject.transform.Rotate(0, 0, 0);
 		}
 
 		if (flagDown) {
 			transform.position = Position;
-			Position.y -= 0.05F;
-		}
-	}
+			Position.y -= 0.1F;
+            if (!rotated){
+                this.gameObject.transform.Rotate(0, 0, 180);
+                rotated = true;
+            }
+        }
+        if (flagLeft) {
+            transform.position = Position;
+            Position.x -= 0.1F;
+            if (!rotated)
+            {
+                this.gameObject.transform.Rotate(0, 0, 90);
+                rotated = true;
+            }
+        }
+        if (flagRight)
+        {
+            transform.position = Position;
+            Position.x += 0.1F;
+            if (!rotated)
+            {
+                this.gameObject.transform.Rotate(0, 0, -90);
+                rotated = true;
+            }
+        }
+    }
 }
