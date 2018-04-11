5dd0d876c445319a256fda671ac7b8d61930dd8a
Anggar Wahyu <anggarwahyu@gmail.com>
2018-04-12 02:45:16 +0700

    Mechanism Update
    Adding some mechanism, with some bug for converting primitive char to KeyCode.

---
+++ Assets/scripts/food.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour {
+
+	Vector2 Position;
+
+	Rigidbody2D pgn;
+	public BoxCollider2D haha;
+
+	// Use this for initialization
+	void Start () {
+		pgn = GetComponent<Rigidbody2D>();
+		haha = GetComponent<BoxCollider2D>();
+	
+		Position = new Vector2(Random.Range(-36, 36), Random.Range(-15, 15));
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo){
+		if(collisionInfo.collider.name == "snakeOne"){
+			Position = new Vector2(Random.Range(-36, 36), Random.Range(-15, 15));
+			pgn.position = Position;
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+		pgn.position = Position;
+	}
+}
--- Assets/scripts/snakeOne.cs
+++ Assets/scripts/snakeOne.cs
@@ -8,79 +8,107 @@ public class snakeOne : MonoBehaviour {
 	bool flagDown 	= false;
     bool flagRight  = false;
     bool flagLeft   = false;
-    bool rotated    = false;
+    int rotateState = 0;
 
-	public Vector2 Position;
-	public float Velocity = 0f;
+    public CircleCollider2D colli;
+
+	Vector2 Position;
+	public float Velocity = 0.5f;
 
 	// Use this for initialization
 	void Start () {
 		Position = new Vector2 (0, 0);
+        colli = GetComponent<CircleCollider2D>();
 	}
-	
+
+    public char buttonUp        = 'w';
+    public char buttonDown      = 's';
+    public char buttonRight     = 'd';
+    public char buttonLeft      = 'a';    
+
+    void randomizeButtonTrigger(){
+        buttonUp = (char)(Random.Range(0, 25) + (int)('a'));
+        buttonDown = (char)((Random.Range(1, 25) + (int)(buttonUp) - (int)('a')) % 26 + (int)('a'));
+
+        buttonLeft = (char)(Random.Range(0, 25) + (int)('a'));
+        while(buttonLeft == buttonUp || buttonLeft == buttonDown) buttonLeft = (char)(Random.Range(0, 25) + (int)('a'));
+        buttonRight = (char)((Random.Range(1, 25) + (int)(buttonLeft) - (int)('a')) % 26 + (int)('a'));
+        while(buttonRight == buttonUp || buttonRight == buttonDown || buttonRight == buttonLeft) buttonRight = (char)(Random.Range(0, 25) + (int)('a'));
+
+        print(buttonUp.ToString() + " " + buttonRight + " " + buttonDown + " " + buttonLeft);
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        // Debug.Log(collisionInfo.collider.name);
+        if(collisionInfo.collider.tag == "Wall"){
+            Destroy(gameObject);
+        }        
+
+        if(collisionInfo.collider.name == "food"){
+            Velocity *= 1.1f;
+            randomizeButtonTrigger();
+            // Debug.Log("Velocity: " + Velocity);
+        }
+    }
+
+    string keString(char x){
+        string haha = x.ToString();
+        return haha;
+    }
+
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+		if (Input.GetKeyDown(keString(buttonUp))) {
 			flagUp = true;
 			flagDown = false;
             flagLeft = false;
             flagRight = false;
-            rotated = false;
         }
-		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+		if (Input.GetKeyDown(keString(buttonDown))) {
 			flagDown = true;
 			flagUp = false;
             flagLeft = false;
             flagRight = false;
-            rotated = false;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKeyDown(keString(buttonRight))) {
             flagRight = true;
             flagLeft = false;
             flagDown = false;
             flagUp = false;
-            rotated = false;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+        if(Input.GetKeyDown(keString(buttonLeft))) {
             flagRight = false;
             flagLeft = true;
             flagDown = false;
             flagUp = false;
-            rotated = false;
         }
 
         if (flagUp) {
 			transform.position = Position;
 			Position.y += 0.1F * Velocity;
-		}
+            this.gameObject.transform.Rotate(0, 0, rotateState);
+            rotateState = 0;
+        }
 
 		if (flagDown) {
-			transform.position = Position;
+            transform.position = Position;
 			Position.y -= 0.1F * Velocity;
-            if (!rotated){
-                this.gameObject.transform.Rotate(0, 0, 180);
-                rotated = true;
-            }
-        }
+            this.gameObject.transform.Rotate(0, 0, rotateState+180);
+            rotateState = 180;
+		}
         if (flagLeft) {
             transform.position = Position;
             Position.x -= 0.1F * Velocity;
-            if (!rotated)
-            {
-                this.gameObject.transform.Rotate(0, 0, 90);
-                rotated = true;
-            }
+            this.gameObject.transform.Rotate(0, 0, rotateState + 90);
+            rotateState = -90;
         }
-        if (flagRight)
-        {
+        if (flagRight) {
             transform.position = Position;
             Position.x += 0.1F * Velocity;
-            if (!rotated)
-            {
-                this.gameObject.transform.Rotate(0, 0, -90);
-                rotated = true;
-            }
+            this.gameObject.transform.Rotate(0, 0, rotateState - 90);
+            rotateState = 90;
         }
     }
 }
